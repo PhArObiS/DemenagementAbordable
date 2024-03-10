6714222e7b3ca97821a43b1411172718d11121e0
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-03-10 14:03:09 -0400

    Map fix and Spelling

--- js/script.js
+++ js/script.js
@@ -1,30 +1,16 @@
-// Initialioze
-function initMap() {
-    // Your location
-    const loc = { lat: 45.524115, lng: -73.6221836 };
-    // Centered map on location
-    const map = new google.maps.Map(document.querySelector('.map'),
-        {
-            zoom: 10,
-            center: loc
-        });
-    // The marker, positioned at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
-}
+// Smooth Scrolling
+$(".navbar-nav a").on("click", function (event) {
+  if (this.hash != "") {
+    event.preventDefault();
+    const hash = this.hash;
 
-// Smooth Scrolling 
-$('.navbar-nav a').on('click', function (event) {
-    if (this.hash != '') {
-        event.preventDefault();
-        const hash = this.hash;
-
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
 });
 
 // Navbar menu opacity
@@ -33,36 +19,36 @@ var navbar = document.getElementById("navbar");
 navbar.style.transition = "opacity 0.3s";
 
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        navbar.style.opacity = "1";
-    } else {
-        navbar.style.opacity = "0.5";
-    }
-    prevScrollpos = currentScrollPos;
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.opacity = "1";
+  } else {
+    navbar.style.opacity = "0.5";
+  }
+  prevScrollpos = currentScrollPos;
 };
 
 // Contact form
 $(document).ready(function () {
-    $("#contact-form").submit(function (event) {
-        event.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var telephone = $("#phone").val();
-        var message = $("#message").val();
-        $.ajax({
-            method: "POST",
-            url: "https://demenagementabordable.com/api/send-lead",
-            data: {
-                name: name,
-                email: email,
-                telephone: telephone,
-                message: message,
-            },
-        }).done(function (msg) {
-            alert("Email Sent");
-        });
+  $("#contact-form").submit(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var telephone = $("#phone").val();
+    var message = $("#message").val();
+    $.ajax({
+      method: "POST",
+      url: "https://demenagementabordable.com/api/send-lead",
+      data: {
+        name: name,
+        email: email,
+        telephone: telephone,
+        message: message,
+      },
+    }).done(function (msg) {
+      alert("Email Sent");
     });
+  });
 });
 
 // MiniScreenNavButton
@@ -70,7 +56,5 @@ const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navmenu = document.getElementsByClassName("nav-menu")[0];
 
 toggleBtn.addEventListener("click", () => {
-    navmenu.classList.toggle("active");
+  navmenu.classList.toggle("active");
 });
-
-
