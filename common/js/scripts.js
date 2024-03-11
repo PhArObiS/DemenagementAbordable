c621570a4033a262ec6cd9000a09e4fea396b7ac
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-03-11 12:37:26 -0400

    en/fr

--- common/js/scripts.js
+++ common/js/scripts.js
@@ -1,6 +1,7 @@
 // Smooth Scrolling
+
 $(".navbar-nav a").on("click", function (event) {
-  if (this.hash != "") {
+  if (this.hash !== "") {
     event.preventDefault();
     const hash = this.hash;
 
@@ -51,10 +52,34 @@ $(document).ready(function () {
   });
 });
 
-// MiniScreenNavButton
-const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
-const navmenu = document.getElementsByClassName("nav-menu")[0];
+// Language switcher
+const langToggle = document.getElementById("langToggle");
 
-toggleBtn.addEventListener("click", () => {
-  navmenu.classList.toggle("active");
-});
+if (langToggle) {
+  langToggle.addEventListener("click", function () {
+    console.log("Language toggle button clicked!");
+
+    // Get the current language from the HTML tag
+    const currentLang = document.documentElement.lang || "en";
+    console.log("Current Language:", currentLang);
+
+    // Determine the new language
+    const newLang = currentLang === "en" ? "fr" : "en";
+    console.log("New Language:", newLang);
+
+    // Generate the new HTML file name based on the language
+    const htmlFileName = newLang === "en" ? "index.html" : "index-fr.html";
+    console.log("HTML File Name:", htmlFileName);
+
+    // Update the location to navigate to the new language version
+    try {
+      const newPath = new URL(htmlFileName, window.location.href).pathname;
+      console.log("New Path:", newPath);
+      location.href = newPath;
+    } catch (error) {
+      console.error("Error generating new URL:", error);
+    }
+  });
+} else {
+  console.error("Language toggle button not found!");
+}
